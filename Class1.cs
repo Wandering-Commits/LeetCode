2750e34c93fb1d57935a92d8fa97ac5c6e7e93a4
Brian Hathaway <brianjhathaway@outlook.com>
2023-05-17 10:26:31 -0500

    Created pull class and implementation in main "Program.Cs" for Question #1: Two Sum

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Two_Sum
+    {
+        public int[] Two_sum_algorithm(int[] nums, int target)
+        {
+            int[] indexes = new int[2];
+            for (int i = 0; i<nums.Length; i++)
+            {
+                for (int j = i+1; j<nums.Length; j++)
+                {
+                    int temp_sum = nums[i] + nums[j];
+                    if(temp_sum == target)
+                    {
+                        indexes[0] = i;
+                        indexes[1] = j;
+                        return indexes;
+                    }
+                }
+            }
+            Console.WriteLine("No two values in the array sum to that target.");
+            return indexes;
+        }
+
+        public void print_array(int[] arr)
+        {
+            Console.Write("[");
+            foreach(var item in arr)
+            {
+                Console.Write(item);
+                if(item != arr[arr.Length - 1])
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine(']');
+        }
+
+        public void Implement_two_sum(Two_Sum imported)
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            imported.print_array(nums);
+            Console.WriteLine("Enter a target value.");
+            int target = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Target value is: " + target);
+            int[] index_locations = imported.Two_sum_algorithm(nums, target);
+            if (index_locations.Any() != true)
+            {
+                Console.Write("The indexes to sum to target value are: ");
+                imported.print_array(index_locations);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using System.Globalization;
+using LeetCode;
+
+// #1 Two Sum
+Two_Sum imported = new();
+imported.Implement_two_sum(imported);
